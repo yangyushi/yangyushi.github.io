22d15f23d83436828996f3e0ae5fb2fd477836c8
Yushi Yang <yangyushi1992@icloud.com>
2020-07-04 17:01:00 +0100

    post update 07-04-20

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,17 +1,76 @@
+function foldTOC() {
+    var toc = document.getElementById('markdown-toc-new');
+    if (toc.style.display === "none") {
+        toc.style.display = "block";
+    } else {
+        toc.style.display = "none";
+    }
+}
+
+
 function moveTOC() {
     var toc = document.getElementById('markdown-toc');
     if (toc) {
-        var side_toc = document.getElementById('side_bar');
-        side_toc.innerHTML += ('<br><p><strong>Table of contents</strong></p>');
-        side_toc.innerHTML += toc.innerHTML;
+        var new_toc = toc.cloneNode(true);
         toc.parentNode.removeChild(toc);
+        new_toc.id = 'markdown-toc-new'
+        var side_toc = document.getElementById('side_bar');
+        side_toc.innerHTML = '<div onclick="foldTOC()" class="toc_click"> Table of Contents </div>';
+        side_toc.appendChild(new_toc);
     }
 }
+
+
+function addClick() {
+    var toc = document.getElementById('markdown-toc-new');
+    if (toc){
+        var children = toc.children;
+        for (var i = 0; i < children.length; i++){
+            var child = children[i];
+            if (child.children.length > 1){
+                var to_click = document.createElement("span");
+                to_click.innerHTML = '<span onclick="foldTOCList(this)" class="fold_click"> + </span>';
+                to_click.id = child.children[0].id;
+                child.insertBefore(to_click, child.children[1]);
+            }
+        }
+    }
+}
+
+function autoFold() {
+    var toc = document.getElementById('markdown-toc-new');
+    if (toc){
+        var children = toc.children;
+        for (var i = 0; i < children.length; i++){
+            var child = children[i];
+            if (child.children.length > 1){
+                var to_click = child.children[1].children[0];
+                foldTOCList(to_click);
+            }
+        }
+    }
+}
+
+function foldTOCList(clicked) {
+    var to_fold = clicked.parentElement.parentElement.children[2];
+    if (typeof to_fold == 'object' && to_fold != null){
+        if (to_fold.style.display === "none") {
+            to_fold.style.display = "block";
+            clicked.innerHTML = ' - ';
+        } else {
+            to_fold.style.display = "none";
+            clicked.innerHTML = ' + ';
+        }
+    }
+}
+
 
 if ( document.addEventListener ) { // Mozilla, Opera, Webkit
     document.addEventListener( 'DOMContentLoaded', function() {
         document.removeEventListener( 'DOMContentLoaded', arguments.callee, false);
         moveTOC();
+        addClick();
+        autoFold();
     }, false );
 } else if ( document.attachEvent ) { // If IE event model is used
     // ensure firing before onload
@@ -19,6 +78,8 @@ if ( document.addEventListener ) { // Mozilla, Opera, Webkit
         if ( document.readyState === 'complete' ) {
             document.detachEvent( 'onreadystatechange', arguments.callee );
             moveTOC();
+            addClick();
+            autoFold();
         }
     });
 }
